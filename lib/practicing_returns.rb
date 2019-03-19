beffe05855def5b01bcbc19dceac8c28b4fec4dd
Gerardo Manzo <programmingmanzo@gmail.com>
2019-03-19 07:38:42 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -6,7 +6,7 @@ def hello(array)
   while i < array.length
   #binding.pry 
     #yield(array[i])
-    #collection << yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
   collection
